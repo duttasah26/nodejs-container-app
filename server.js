e205821c5207de549effb0f9c4eb41dd99d34680
duttasah <duttasah@sheridancollege.ca>
2024-12-13 12:31:41 -0500

    Made changes added studentnumber to print

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Dockerized Node.js App! Sahil Updated this');
+    res.send('Hello, Dockerized Node.js App! Sahil Dutta 991684086');
 });
 
 app.listen(PORT, () => {
